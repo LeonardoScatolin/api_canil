2687faf773ec9e3f7b9c213c47797acfea26f87d
LeonardoScatolin <leonardo.scatolin@gmail.com>
2024-11-23 13:52:17 -0300

    atualizando db

--- src/db/db.js
+++ src/db/db.js
@@ -5,11 +5,11 @@ const knex = require("knex");
 const connection = knex({
   client: 'mysql2',
   connection: {
-    host: process.env.HOST,
-    port: process.env.DB_PORT,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    host: 'database-canil.cpqyoc46ahv7.us-east-2.rds.amazonaws.com',
+    port: '3306',
+    user: 'admin',
+    password: 'admin',
+    database: 'api_canil',
   },
 });
 
